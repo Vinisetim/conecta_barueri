16861ab2411e0dc7e01d98bf681cb4cc710b31ee
vinicius-dec <viniciusbellonidesouza@gmail.com>
2026-05-19 19:30:32 -0300

    Adicionando o escopo da pagina login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
-
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login')
 def login():
     # Por enquanto, apenas para não dar erro
-    return "Nenhum Arquivo para acessar"
+    return render_template('auth/login.html')
+
+
+#"Nenhum Arquivo para acessar"
 
 #render_template(auth/login.html)
 
