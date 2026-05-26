1d09f0eb8779ce8cd0d32ff1dc5e4abe1fd54f97
Vinisetim <viniciusgueimis@gmail.com>
2026-05-25 21:05:04 -0300

    Configurações iniciais banco

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm #A classe contém automaticamente a proteção CSRF no formulário
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, email, length, Email, Length
+
+
+class LoginForm(FlaskForm):
+    """Formulário de Autenticação, Valida as Credenciais antes de consultas ao banco"""
+
+    #E-mail validado por formato e com preenchimento obrigatório
+    email = StringField('Usuário', validators=[
+        DataRequired(message='O email é obrigatório'),
+        Email(message='Digite um e-mail válido'),
+        Length(max=150, message='E-mail muito longo')
+    ])
+
+    #PasswordField oculta o texto automaticamente no HTML
+    senha = PasswordField('Senha', validators=[
+        DataRequired(message='A Senha é Obrigatória'),
+        Length(min=4, message='A senha deve ter ao minímo 4 caracteres'),
+    ])
+
+    lembrar = BooleanField('Lembrar de mim')
+
+    submit = SubmitField('Entrar')
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,11 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user,login_required
+from app.models import Usuario
+from app import bcrypt
+from app.forms import LoginForm
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # Por enquanto, apenas para não dar erro
-    return "Nenhum Arquivo para acessar"
+    """Rota de Autenticação
+        GET => exibe o formulário de login
+        POST => Valida credenciais e inicia sessão"""
 
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        #Busca o usuário pelo email digitado no formulário
+        usuario = Usuario.query.filter_by(email=form.email.data).first()
+
+        #Verifica se o usuário existe e a senha bate com o Hash
+        if usuario and bcrypt.check_password_hash(usuario.senha.senha, form.senha.data):
+            login_user(usuario, remember=form.lembrar.data)
+
+            #Se o usuário tentou acessar uma página protegida, redireciona para ela, se não vai para main
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+
+        flash('Credenciais inválidas. Tente novamente', 'danger')
+    return render_template('auth/login.html', form=form)
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    """Encerra a sessão do usuario autenticado e redireciona para a landing page"""
+    logout_user()
+    return redirect(url_for('main.index'))
 #render_template(auth/login.html)
 
